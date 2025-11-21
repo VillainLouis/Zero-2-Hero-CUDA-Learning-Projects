0876e07928ae520149ef088ab06047b5f614994c
v.louis <villainlouis@mail.ustc.edu.cn>
2025-11-21 22:52:15 +0800

    implemented reduce v0 (TODO: fix warp divergence issue)

--- 01_Reduce/reduce_v0_global_memory.cu
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <stdlib.h>
-
-int main() {
-    printf("Hello, CUDA Global Memory Reduction!\n");
-    return 0;
-}
--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
 ## 01_Reduce
 TODO: Notes
+参考文章：https://zhuanlan.zhihu.com/p/426978026
+代码链接：https://github.com/Liu-xiandong/How_to_optimize_in_GPU
+实验平台：A100
